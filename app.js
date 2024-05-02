f34e2a27492273a7cfce9ba4dbb4f1e7d56a2d18
Victoria Kowalk <victoriakowalk@gmail.com>
2024-05-02 20:32:03 -0300

    Version 2

---
+++ app.js
@@ -0,0 +1,61 @@
+//animaciones de los graficos
+function cargarAnimaciones(){
+    $(function(){
+        $('.chart1').easyPieChart({
+            size:160,
+            barColor: "#0a8a8a",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#050505",
+            lineCap: "circle",
+            animate: 2000,
+        })
+    });
+    $('.chart2').easyPieChart({
+        size:160,
+        barColor: "#0a8a8a",
+        scaleLength:0,
+        lineWidth:15,
+        tackColor:"#050505",
+        lineCap: "circle",
+        animate: 2000,
+    });
+    $('.chart3').easyPieChart({
+        size:160,
+        barColor: "#0a8a8a",
+        scaleLength:0,
+        lineWidth:15,
+        tackColor:"#050505",
+        lineCap: "circle",
+        animate: 2000,
+    });
+    $('.chart4').easyPieChart({
+        size:160,
+        barColor: "#0a8a8a",
+        scaleLength:0,
+        lineWidth:15,
+        tackColor:"#050505",
+        lineCap: "circle",
+        animate: 2000,
+    });
+    $('.chart5').easyPieChart({
+        size:160,
+        barColor: "#0a8a8a",
+        scaleLength:0,
+        lineWidth:15,
+        tackColor:"#050505",
+        lineCap: "circle",
+        animate: 2000,
+    });
+    $('.chart6').easyPieChart({
+        size:160,
+        barColor: "#0a8a8a",
+        scaleLength:0,
+        lineWidth:15,
+        tackColor:"#050505",
+        lineCap: "circle",
+        animate: 2000,
+    });
+}
+
+cargarAnimaciones();
